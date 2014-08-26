ac71d75082338ad7fdc005f07971cac4a57039d9
Joel Hughes <joelhughes06@netscape.net>
2014-08-26 15:25:12 -0600

    082614 1525 merge

--- main.rb
+++ main.rb
@@ -4,31 +4,106 @@ require 'sinatra/contrib/all'
 
 set :sessions, true
 
-get '/home' do
-	"Welcome home. It sure has been a while, hasn\'t it? Is this working?"
+helpers do
+	def calculate_total(cards)
+		arr = cards.map{|element| element[1]} #this directs the program to focus on second element of the card i.e. the '4' in ['S', '4']
+	
+		total = 0
+		arr.each do |a|
+			if a == "A"
+				total += 11
+			else
+				total += a.to_i == 0 ? 10 : a.to_i # this takes care of turning J, Q, K, A into 10
+			end
+	end
+
+			arr.select{|element| element == "A"}.count.times do
+				break if total <= 21
+				total -= 10
+			end
+
+			total 
+		end
+
+def card_image(card) #['H', '3']
+	suit = case card[0]
+		when 'H' then 'hearts'
+		when 'D' then 'diamonds'
+		when 'C' then 'clubs'
+		when 'S' then 'spades'
+	end
+
+	value = card[1]
+	if ['J', 'Q', 'K', 'A'].include?(value)
+		value = case card[1]
+		when 'J' then 'jack'
+		when 'Q' then 'queen'
+		when 'K' then 'king'
+		when 'A' then 'ace'
+	end
 end
 
-
-get '/inline' do
-	"Hi, directly from the action!"
+	"<img src='/images/cards/#{suit}_#{value}.jpg'>"
 end
 
-get '/template' do 
-	erb :mytemplate
+	end
+
+before do
+	@show_hit_or_stay_buttons = true
+end
+#calculate total(session(:dealers cards)
+get '/' do
+	if session[:player_name]
+		redirect '/game'
+	else
+		redirect '/new_player'
+	end
 end
 
-get '/nested_template' do
-	erb :"/users/profile"
+#post '/' do
+#	params['username']
+#end
+
+get '/new_player' do
+	erb :new_player
 end
 
-get '/nothere' do
-	redirect '/inline'
+post '/new_player' do
+	session[:player_name] = params[:player_name]
+	redirect '/game'
+	#progress to the game
 end
 
-get '/form' do
-	erb :form
+get '/game' do
+	#create a deck and put it into a session
+	suits = ['H', 'D', 'C', 'S']
+	values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+	session[:deck] = suits.product(values).shuffle! # [['H', '9']m ['C', 'K'] ... ]
+	#deal cards
+	session[:dealer_cards] = []
+	session[:player_cards] = []
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	
+	erb :game
 end
 
-post '/myaction' do
-	params['username']
+post '/game/player/hit' do
+	#deal the next card if clicks 'Hit'
+		session[:player_cards] << session[:deck].pop
+		if calculate_total(session[:player_cards]) > 21
+		@error = "Sorry, you busted."
+		@show_hit_or_stay_buttons = false
+
+	end
+
 end
+
+post '/game/player/stay' do
+	@success = "You have chosen to stay."
+	@show_hit_or_stay_buttons = false
+	erb :game
+end
+
---
+++ week3httpblackjackOLD/week_3_prep/main.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/contrib/all'
+
+set :sessions, true
+
+get '/home' do
+	"Welcome home. It sure has been a while, hasn\'t it? Is this working?"
+end
+
+
+get '/inline' do
+	"Hi, directly from the action!"
+end
+
+get '/template' do 
+	erb :mytemplate
+end
+
+get '/nested_template' do
+	erb :"/users/profile"
+end
+
+get '/nothere' do
+	redirect '/inline'
+end
+
+get '/form' do
+	erb :form
+end
+
